a65983f29fed66466f79981e89c159804692a638
IgorVerchyk <sabyr.ivua@gmail.com>
2020-07-06 21:36:31 +0300

    fixed stupid mistakes

--- src/js/theme.js
+++ src/js/theme.js
@@ -7,23 +7,29 @@ const themeSwitch = document.querySelector('.js-switch-input');
 const savedTheme = localStorage.getItem('theme');
 
 themeSwitch.addEventListener('change', setTheme);
+function changeThemeForDark() {
+  pageBody.classList.remove(Theme.LIGHT);
+  localStorage.setItem('theme', Theme.DARK);
+  pageBody.classList.add(Theme.DARK);
+}
+function changeThemeForLight() {
+  pageBody.classList.remove(Theme.DARK);
+  localStorage.setItem('theme', Theme.LIGHT);
+  pageBody.classList.add(Theme.LIGHT);
+}
 
 function setTheme(event) {
   if (event.target.checked) {
-    pageBody.classList.remove(Theme.LIGHT);
-    localStorage.setItem('theme', Theme.DARK);
-    pageBody.classList.add(Theme.DARK);
+    changeThemeForDark();
   } else {
-    pageBody.classList.remove(Theme.DARK);
-    localStorage.setItem('theme', Theme.LIGHT);
-    pageBody.classList.add(Theme.LIGHT);
+    changeThemeForLight();
   }
 }
 
 export function checkForTheme() {
   if (savedTheme) {
-    pageBody.classList.add(storagedTheme);
-    if (storagedTheme === Theme.DARK) {
+    pageBody.classList.add(savedTheme);
+    if (savedTheme === Theme.DARK) {
       themeSwitch.checked = true;
     } else {
       ('');
